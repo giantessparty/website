de37211bf4c7c94332948abfb3ccd68bcad56161
giantessparty <giantessparty@protonmail.com>
2018-11-18 12:18:16 -0700

    Added working bench app.

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact';
+import { Router } from 'preact-router';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div id="app">
+				<h1>Hello, app!</h1>
+			</div>
+		);
+	}
+}
